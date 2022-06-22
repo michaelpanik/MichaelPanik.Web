544bb082ea821b4632001c26331c13605898562d
Michael Panik <michael@michael-panik.com>
2022-06-21 23:14:02 -0500

    Fix build blocker

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,33 +18,32 @@ exports.createPages = async ({ graphql, actions }) => {
 
   posts.data.allSanityPost.nodes.forEach((post) => {
     createPage({
-      path: `blog/${post.slug.current}/`,
+      path: `blog/${post?.slug?.current || post?.id}/`,
       component: path.resolve(`./src/templates/post.tsx`),
       context: post,
     });
   });
 
-  // const portfolioItems = await graphql(`
-  //   {
-  //     allSanityPortfolio {
-  //       nodes {
-  //         id
-  //         slug {
-  //           current
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+  const portfolioItems = await graphql(`
+    {
+      allSanityPortfolio {
+        nodes {
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
 
-  // portfolioItems.data.allSanityPortfolio.nodes.forEach((portfolioItem) => {
-  //   createPage({
-  //     path: `portfolio/${portfolioItem.slug.current || portfolioItem.id}/`,
-  //     component: path.resolve(`./src/templates/portfolio.tsx`),
-  //     context: portfolioItem,
-  //   });
-  // });
+  portfolioItems.data.allSanityPortfolio.nodes.forEach((portfolioItem) => {
+    createPage({
+      path: `portfolio/${portfolioItem?.slug?.current || portfolioItem?.id}/`,
+      component: path.resolve(`./src/templates/portfolio.tsx`),
+      context: portfolioItem,
+    });
+  });
 
-  // return Promise.all([posts, portfolioItems]);
-  return Promise.all([posts]);
+  return Promise.all([posts, portfolioItems]);
 };
