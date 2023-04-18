d17abd0187117f5cfd59b6fd0e609e95d804a4eb
michaelpanik <michael@michael-panik.com>
2023-04-18 08:12:47 -0500

    Prepare for launch

--- src/hooks/useNavigation.js
+++ src/hooks/useNavigation.js
@@ -1,7 +1,7 @@
 const useNavigation = () => {
   return [
     { url: "/about/", label: "About" },
-    { url: "/portfolio/", label: "Portfolio" },
+    // { url: "/portfolio/", label: "Portfolio" },
     { url: "/blog/", label: "Blog" },
     { url: "/resume.pdf", label: "Resume" },
     { url: "/contact/", label: "Contact" },
--- src/hooks/useSocialLinks.js
+++ src/hooks/useSocialLinks.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import { faGithub, faInstagram, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const useSocialLinks = () => {
-    return [
-        { label: 'Twitter', url: 'https://twitter.com/michaelpanik', icon: <FontAwesomeIcon icon={faTwitter} /> },
-        { label: 'Instagram', url: 'https://instagram.com/frontend.io', icon: <FontAwesomeIcon icon={faInstagram} /> },
-        { label: 'LinkedIn', url: 'https://linkedin.com/in/michaelpanik', icon: <FontAwesomeIcon icon={faLinkedin} /> },
-        { label: 'GitHub', url: 'https://github.com/michaelpanik', icon: <FontAwesomeIcon icon={faGithub} /> },
-    ]
-}
+  return [
+    // { label: 'Twitter', url: 'https://twitter.com/michaelpanik', icon: <FontAwesomeIcon icon={faTwitter} /> },
+    {
+      label: "Instagram",
+      url: "https://instagram.com/frontend.io",
+      icon: <FontAwesomeIcon icon={faInstagram} />,
+    },
+    {
+      label: "LinkedIn",
+      url: "https://linkedin.com/in/michaelpanik",
+      icon: <FontAwesomeIcon icon={faLinkedin} />,
+    },
+    {
+      label: "GitHub",
+      url: "https://github.com/michaelpanik",
+      icon: <FontAwesomeIcon icon={faGithub} />,
+    },
+  ];
+};
 
-export default useSocialLinks
+export default useSocialLinks;
